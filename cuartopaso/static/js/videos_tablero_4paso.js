aadffc5350c2860dc0a09f9c676afefd69687589
Aaron Contreras <139277953+AaronOmarContreras@users.noreply.github.com>
2025-02-20 18:34:27 -0600

    Ajuste a JS

--- cuartopaso/static/js/videos_tablero_4paso.js
+++ cuartopaso/static/js/videos_tablero_4paso.js
@@ -1,37 +1,34 @@
-const modal = document.getElementById('modal');
-const closeButton = document.querySelector('close-button');
-const modalTitle = document.querySelector('modal-title');
-const modalVideo = document.getElementById('modal-video-src');
+const modal = document.getElementById("modal");
+const closeButton = document.querySelector(".close-button");
+const modalTitle = document.getElementById("modal-title");
+const modalVideo = document.getElementById("modal-video-src");
 
 function openModal(event) {
+  let card = event.target.closest(".card");
+  if (!card) return;
 
-    let card = event.target.closest('.card');
-    if (!card) return;
+  const university = card.getAttribute("data-university");
+  const VideoUrl = card.getAttribute("data-video");
 
-    const university = card.getAttribute('data-university');
-    const VideoUrl = card.getAttribute('data-video');
+  modalTitle.textContent = university;
+  modalVideo.src = VideoUrl;
 
-
-    modalTitle.textContent = university;
-    modalVideo.src = VideoUrl;
-
-    modal.style.display = 'block';
-
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = 'none';
-    modalVideo.src = '';
+  modal.style.display = "none";
+  modalVideo.src = "";
 }
 
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', openModal);
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", openModal);
 });
 
-closeButton.addEventListener('click', closeModal);
+closeButton.addEventListener("click", closeModal);
 
-window.addEventListener('click', function (event) {
-    if (event.target == modal) {
-        closeModal();
-    }
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
 });
--- sextopaso/static/js/tablero_6paso.js
+++ sextopaso/static/js/tablero_6paso.js
@@ -1,67 +1,67 @@
 // Obtener elementos del DOM
-const modal = document.getElementById('modal');
-const modalTitle = document.querySelector('modal-title');
-const closeButton = document.querySelector('.close-button');
-const pdfIframe = document.getElementById('modal-pdf');
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modal-title");
+const closeButton = document.querySelector(".close-button");
+const pdfIframe = document.getElementById("modal-pdf");
 let pdfDoc = null;
-
 
 // Load and render PDF with PDF.js library
 function loadPdf(url) {
-    const loadingTask = pdfjsLib.getDocument(url);
-    loadingTask.promise.then(function (pdf) {
-        pdfDoc = pdf;
-        return pdf.getPage(1);
-    }).then(function (page) {
-        const context = canvas.getContext("2d");
-        const viewport = page.getViewport({ scale: 1.5 });
+  const loadingTask = pdfjsLib.getDocument(url);
+  loadingTask.promise
+    .then(function (pdf) {
+      pdfDoc = pdf;
+      return pdf.getPage(1);
+    })
+    .then(function (page) {
+      const context = canvas.getContext("2d");
+      const viewport = page.getViewport({ scale: 1.5 });
 
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
 
-        page.render({ canvasContext: context, viewport: viewport });
+      page.render({ canvasContext: context, viewport: viewport });
     });
 }
 
 // Función para abrir la ventana modal con la información de la tarjeta clickeada
 function openModal(event) {
-    // Verificar si el elemento clickeado es una tarjeta o está dentro de una
-    let card = event.target.closest('.card');
-    if (!card) return;
+  // Verificar si el elemento clickeado es una tarjeta o está dentro de una
+  let card = event.target.closest(".card");
+  if (!card) return;
 
-    // Extraer información de los atributos de datos
-    const university = card.getAttribute('data-university');
-    const PDFUrl = card.getAttribute('data-pdf');
+  // Extraer información de los atributos de datos
+  const university = card.getAttribute("data-university");
+  const PDFUrl = card.getAttribute("data-pdf");
 
-    // Rellenar el contenido de la ventana modal
-    modalTitle.textContent = university;
+  // Rellenar el contenido de la ventana modal
+  modalTitle.textContent = university;
 
-    if (pdfDoc) {
-        loadPdf(pdfDoc, PDFUrl);
-    }
+  if (pdfDoc) {
+    loadPdf(pdfDoc, PDFUrl);
+  }
 
-    // Mostrar la ventana modal
-    modal.style.display = 'block';
+  // Mostrar la ventana modal
+  modal.style.display = "block";
 }
 
 // Función para cerrar la ventana modal
 function closeModal() {
-    modal.style.display = 'none';
-    modalVideo.src = '';
+  modal.style.display = "none";
+  modalVideo.src = "";
 }
 
 // Event listener para abrir la ventana modal al hacer clic en una tarjeta
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', openModal);
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", openModal);
 });
 
 // Event listener para cerrar la ventana modal al hacer clic en el botón de cierre
-closeButton.addEventListener('click', closeModal);
+closeButton.addEventListener("click", closeModal);
 
 // Event listener para cerrar la ventana modal al hacer clic fuera del contenido modal
-window.addEventListener('click', function (event) {
-    if (event.target == modal) {
-        closeModal();
-    }
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
 });
-
