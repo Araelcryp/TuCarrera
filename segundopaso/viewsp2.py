e35171f9e6278859b6e78d1c1864bdc4d53d184c
Arael Amador <azxlherk@gmail.com>
2025-03-01 22:42:57 -0600

    Generacion de constancias

--- segundopaso/viewsp2.py
+++ segundopaso/viewsp2.py
@@ -3,33 +3,30 @@ from django.contrib.auth.decorators import login_required
 from .models import TestResult
 from django.http import JsonResponse
 
+CATEGORY_DESCRIPTIONS = {
+    "rojo": "ÁREA PROFESIONAL: CIENCIAS SOCIALES - CARRERAS EN ADMINISTRACIÓN Y NEGOCIOS",
+    "morado": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN INGENIERÍA Y MANUFACTURA",
+    "azul": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN TECNOLOGÍAS DE LA INFORMACIÓN",
+    "verde": "ÁREA PROFESIONAL: HUMANIDADES Y ARTES - CARRERAS EN HUMANIDADES Y ARTES",
+    "amarillo": "ÁREA PROFESIONAL: CIENCIAS BIOLÓGICAS, QUÍMICAS Y DE LA SALUD - CARRERAS EN CIENCIAS DE LA SALUD",
+    "gris": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN CIENCIAS NATURALES, MATEMÁTICA Y ESTADÍSTICA",
+    "menta": "ÁREA PROFESIONAL: ARTES Y HUMANIDADES - CARRERAS EN EDUCACIÓN"
+}
 
 @login_required
 def segundopaso(request):
-    profile = request.user.profile
-    # Verificar si todos los progresos están al 100%
-    if (
-        profile.progreso_testInteligencias < 100 or
-        profile.progreso_inteligencias < 100 
-    ):
-        return redirect('/mis-intereses-s2')  # Redirige de nuevo si no ha completado todo
-    
     return render (request,'segundopaso.html')
 
+@login_required
+def frase_segundopaso(request):
+    return render (request,'frase_segundopaso.html')
 
 @login_required
 def presentacion_segundopaso(request):
-    profile = request.user.profile
-    profile.progreso_presentacionsegundopaso = 100
-    profile.save()
     return render (request,'presentacion_segundopaso.html')
 
 @login_required
 def instrucciones_segundopaso(request):
-    profile = request.user.profile
-    profile.progreso_testsegundopaso = 100
-    profile.save()
-    
     # Verificar si el usuario tiene resultados guardados
     test_completed = TestResult.objects.filter(user=request.user).exists()
     
@@ -49,27 +46,28 @@ def save_results(request):
         if category and score:
             result = TestResult(user=user, category=category, score=int(score))
             result.save()
-            return JsonResponse({"message": f"Resultado guardado: {category} ({score})"}, status=200)
+
+            description = CATEGORY_DESCRIPTIONS.get(category, "Categoría desconocida")
+
+            return JsonResponse({
+                "category": category,
+                "description": description,
+                "score": score
+            }, status=200)
         else:
             return JsonResponse({"error": "Datos incompletos"}, status=400)
     return JsonResponse({"error": "Método no permitido"}, status=405)
 
-CATEGORY_DESCRIPTIONS = {
-    "rojo": "ÁREA PROFESIONAL: CIENCIAS SOCIALES - CARRERAS EN ADMINISTRACIÓN Y NEGOCIOS",
-    "morado": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN INGENIERÍA Y MANUFACTURA",
-    "azul": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN TECNOLOGÍAS DE LA INFORMACIÓN",
-    "verde": "ÁREA PROFESIONAL: HUMANIDADES Y ARTES - CARRERAS EN HUMANIDADES Y ARTES",
-    "amarillo": "ÁREA PROFESIONAL: CIENCIAS BIOLÓGICAS, QUÍMICAS Y DE LA SALUD - CARRERAS EN CIENCIAS DE LA SALUD",
-    "gris": "ÁREA PROFESIONAL: CIENCIAS FÍSICAS Y MATEMÁTICAS - CARRERAS EN CIENCIAS NATURALES, MATEMÁTICA Y ESTADÍSTICA",
-    "menta": "ÁREA PROFESIONAL: ARTES Y HUMANIDADES - CARRERAS EN EDUCACIÓN"
-}
-
 @login_required
-def resultados_segundopaso(request):
-    # Obtener el resultado más alto del usuario
+def resultados_segundo_paso(request):
     best_result = TestResult.objects.filter(user=request.user).order_by('-score').first()
 
-    if best_result:
-        best_result.description = CATEGORY_DESCRIPTIONS.get(best_result.category, "No se ha podido determinar una categoría.")
+    context = {
+        "result": {
+            "category": best_result.category if best_result else None,
+            "description": CATEGORY_DESCRIPTIONS.get(best_result.category, "No se ha podido determinar una categoría.") if best_result else None,
+            "score": best_result.score if best_result else None
+        } if best_result else None
+    }
 
-    return render(request, "resultados_segundopaso.html", {"result": best_result})
+    return render(request, "resultados_segundopaso.html", context)
